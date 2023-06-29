d711772b19735cb634b8b933c7fcfa0905eb8f90
Silver0x10 <smanganar@gmail.com>
2023-06-29 12:26:25 +0200

    training, validation and testing with logs/tensorboard configutarion; forward specification

--- src/simpleLSTM.py
+++ src/simpleLSTM.py
@@ -18,9 +18,12 @@ class simpleLSTM(pl.LightningModule):
         self.lstm = nn.LSTM(1024, 512)     
         self.decoder = nn.Sequential(nn.Linear(512, 1024), self.relu, nn.Linear(1024, 4096), self.relu, nn.Unflatten(1, (64,64)))
 
-    def forward(self, x):
-        # TODO something like the training step
-        return torch.zeros((1, 64,64))
+    def forward(self, frame, h = None):
+        encoded_frame = self.encoder(frame)
+        lstm_out, h = self.lstm(encoded_frame, h)
+        lstm_out = self.relu(lstm_out)
+        out = self.decoder(lstm_out)
+        return (out, h)
     
     def training_step(self, batch, batch_idx):
         x, y = batch['frames'], batch['y'].float()
@@ -35,12 +38,42 @@ class simpleLSTM(pl.LightningModule):
         out = self.decoder(lstm_out)
 
         loss = self.loss(out, y)
-        self.log("mse", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        # self.log("mse", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log("train_loss", loss, on_epoch=True)
         return loss
     
-    # def validation_step(self, batch, batch_idx):
+    def validation_step(self, batch, batch_idx):
+        x, y = batch['frames'], batch['y'].float()
+        
+        h = None
+        lstm_out = None
+        for frame_nr in range(x.shape[1]):
+            frame = x[:, frame_nr, :, :].view(x.size(0), -1).float()
+            encoded_frame = self.encoder(frame)
+            lstm_out, h = self.lstm(encoded_frame, h)
+        lstm_out = self.relu(lstm_out)
+        out = self.decoder(lstm_out)
+
+        loss = self.loss(out, y)
+        self.log("valid_loss", loss, on_epoch=True)
+        return loss
     
-    # def test_step(self, batch, batch_idx):
+    def test_step(self, batch, batch_idx):
+        x, y = batch['frames'], batch['y'].float()
+        
+        h = None
+        lstm_out = None
+        for frame_nr in range(x.shape[1]):
+            frame = x[:, frame_nr, :, :].view(x.size(0), -1).float()
+            encoded_frame = self.encoder(frame)
+            lstm_out, h = self.lstm(encoded_frame, h)
+        lstm_out = self.relu(lstm_out)
+        out = self.decoder(lstm_out)
+
+        loss = self.loss(out, y)
+        self.log("test_loss", loss, on_epoch=True)
+        return loss
+        
     
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=1e-3)
