f8de5705b98cd9f34d0bc8d3764bb0771bbda73d
Silver0x10 <smanganar@gmail.com>
2023-07-07 12:17:53 +0200

    TAU module implementation [Co-authored-by: Edoardo Colonna <Colonna17@users.noreply.github.com>]

--- src/ConvTAU.py
+++ src/ConvTAU.py
@@ -4,13 +4,46 @@ import torch
 from torch import optim, nn
 import lightning.pytorch as pl
 
+from src.parameters import ParamsConvTAU
+
 class TAU(pl.LightningModule):
     def __init__(self, params):
         super().__init__()
+        k_s = params.kernel_size
+        dim = params.frames_per_sample
+        dilation = params.dilation 
+        
+        d_k = 2 * dilation - 1
+        d_p = (d_k - 1) // 2
+        dd_k = k_s // dilation + ((k_s // dilation) % 2 - 1)
+        dd_p = (dilation * (dd_k - 1) // 2)
+        
+        # Statical Attention Modules
+        self.dw_conv = nn.Conv2d(dim, dim, kernel_size=d_k, padding=d_p, groups=dim)
+        self.dw_d_conv = nn.Conv2d(dim, dim, dd_k, stride=1, padding=dd_p, groups=dim, dilation=dilation)
+        self.onebyone_conv = nn.Conv2d(dim, dim, 1)
+        
+        # Dynamical Attention
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(dim, params.fc_hidden_dim, bias=False), # reduction
+            nn.ReLU(True),
+            nn.Linear(params.fc_hidden_dim, dim, bias=False), # expansion
+            nn.Sigmoid()
+        )
 
     
     def forward(self, x):
-        pass
+        skip = x.clone()
+
+        # Statical Attention
+        sa = self.onebyone_conv( self.dw_d_conv( self.dw_conv(x) ) )
+        
+        # Dynamical Attention
+        da = self.avgpool(x).view(x.shape[0], x.shape[1])
+        da = self.fc(da).view(x.shape[0], x.shape[1], 1, 1)
+        
+        return sa * da * skip
     
         
 class Encoder(pl.LightningModule):
@@ -80,11 +113,27 @@ class ConvTAU(pl.LightningModule):
         self.loss_fn = nn.MSELoss();
 
         self.encoder = Encoder(params)
-        
-        # TODO tau here
-        
+        self.tau = TAU(params)
         self.decoder = Decoder(params)
         
+    
+    def forward(self, frames):
+        frames = frames.unsqueeze(0)
+        B, T, H, W = frames.shape
+        frames = frames.view(B*T, 1, H, W)
+        
+        h, skip = self.encoder(frames)
+        BT, C_, H_, W_ = h.shape
+        
+        h = h.view(B, T*C_, H_, W_)
+        tau_out = self.tau(h)
+        tau_out = tau_out.view(B*T, C_, H_, W_)
+        
+        out = self.decoder(tau_out, skip)
+        out = out.view(B, T, 1, H, W)
+        
+        return out.squeeze(0)
+    
         
     def training_step(self, batch, batch_idx):
         x, y = batch['frames'].float(), batch['y'].float()
@@ -92,9 +141,13 @@ class ConvTAU(pl.LightningModule):
         x = x.view(B*T, 1, H, W)
         
         h, skip = self.encoder(x)
+        BT, C_, H_, W_ = h.shape
         
-        out = self.decoder(h, skip)
-        print(out.shape)
+        h = h.view(B, T*C_, H_, W_)
+        tau_out = self.tau(h)
+        tau_out = tau_out.view(B*T, C_, H_, W_)
+        
+        out = self.decoder(tau_out, skip)
         out = out.view(B, T, 1, H, W)
         print(out.shape)
 
@@ -108,5 +161,6 @@ class ConvTAU(pl.LightningModule):
         return optimizer
     
     def loss(self, pred, y):
+        # TODO KULLBACK-LEIBLER divergence
         return self.loss_fn(pred, y)
         
--- src/parameters.py
+++ src/parameters.py
@@ -9,10 +9,16 @@ W = 1  # Width
 mid_size = 1  # Dimension of smaller layers in Encoder/Decoder
 out_size = 1  # Dimension of the output of the Encoder. It will be the input of TAU
 
+MOVING_MNIST_TOTAL_FRAMES = 20
+MOVING_MNIST_INPUT_FRAMES = 10
 
 class ParamsConvTAU():
     def __init__(self):
         self.batch_size = 5
+        self.frames_per_sample = MOVING_MNIST_INPUT_FRAMES
         self.channels = 1
         
         self.kernel_size = 3
+        
+        self.dilation = 3
+        self.fc_hidden_dim = 4
