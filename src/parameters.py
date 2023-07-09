6c56b8e228023468215b320a7377634c903773e0
Silver0x10 <smanganar@gmail.com>
2023-07-09 14:02:11 +0200

    reordering hyperparams

--- src/parameters.py
+++ src/parameters.py
@@ -1,15 +1,10 @@
-# Per ora metto rtutti uguali ad 1 solo per poterli chiamare e scrivere il codice, poi vanno cambiati coerentemente con i valori del dataset
-
-B = 1  # Batch size, quindi numero di video
-T = 1  # n° di frames per video
-C = 1  # n° di canali
-H = 1  # Height
-W = 1  # Width
-
-mid_size = 1  # Dimension of smaller layers in Encoder/Decoder
-out_size = 1  # Dimension of the output of the Encoder. It will be the input of TAU
 
 MOVING_MNIST_TOTAL_FRAMES = 20
+CHANNELS = 1
+HEIGHT = 64
+WIDTH = 1
+
+BATCH_SIZE = 8
 MOVING_MNIST_INPUT_FRAMES = 10
 
 class ParamsConvTAU():
