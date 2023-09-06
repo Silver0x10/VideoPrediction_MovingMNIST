1c3fb56bc772320ad8743a330e0a215c95ea5374
Silver0x10 <smanganar@gmail.com>
2023-09-06 13:58:13 +0200

    trying to make the params definition and sharing more robust

--- src/parameters.py
+++ src/parameters.py
@@ -1,19 +1,41 @@
 
-MOVING_MNIST_TOTAL_FRAMES = 20
-CHANNELS = 1
-HEIGHT = 64
-WIDTH = 1
 
-BATCH_SIZE = 32
-MOVING_MNIST_INPUT_FRAMES = 10
+shared_params = {
+    'MOVING_MNIST_TOTAL_FRAMES': 20,
+    'MOVING_MNIST_INPUT_FRAMES': 10,
+    'CHANNELS': 1,
+    'HEIGHT': 64,
+    'WIDTH': 64
+}
+
+class ParamsSimpleLSTM():
+    def __init__(self):
+        self.frames_per_sample = shared_params['MOVING_MNIST_INPUT_FRAMES']
+
+        self.batch_size = 16
+        self.training_epochs = 5
+        
+        self.learning_rate = 0.01
+        
+class ParamsConvLSTM():
+    def __init__(self):
+        self.frames_per_sample = shared_params['MOVING_MNIST_INPUT_FRAMES']
+
+        self.batch_size = 16
+        self.training_epochs = 5
+        
+        self.learning_rate = 0.01
 
 class ParamsConvTAU():
     def __init__(self):
+        self.channels = shared_params['CHANNELS']
+        self.frames_per_sample = shared_params['MOVING_MNIST_INPUT_FRAMES']
+
+        self.batch_size = 16
+        self.training_epochs = 5
+        
         self.learning_rate = 0.01
         self.weight_decay = 0.05
-        self.batch_size = 5
-        self.frames_per_sample = MOVING_MNIST_INPUT_FRAMES
-        self.channels = 1
         
         self.kernel_size = 3
         
