f23b59b642c6e7c71054f9c2230bc01742e46e97
Silver0x10 <smanganar@gmail.com>
2023-09-23 13:18:28 +0200

    completing full original model

--- src/ConvTAU.py
+++ src/ConvTAU.py
@@ -1,4 +1,5 @@
 
+import math
 import torch
 from torch import optim, nn
 import lightning.pytorch as pl
@@ -141,7 +142,39 @@ class TemporalAttention(nn.Module):
         if self.attn_shortcut:
             x = x + shortcut
         return x
-    
+
+class DWConv(nn.Module):
+    def __init__(self, dim=768):
+        super(DWConv, self).__init__()
+        self.dwconv = nn.Conv2d(dim, dim, 3, 1, 1, bias=True, groups=dim)
+
+    def forward(self, x):
+        x = self.dwconv(x)
+        return x 
+
+class MixMlp(nn.Module):
+    def __init__(self,
+                 in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Conv2d(in_features, hidden_features, 1)  # 1x1
+        self.dwconv = DWConv(hidden_features)                  # CFF: Convlutional feed-forward network
+        self.act = act_layer()                                 # GELU
+        self.fc2 = nn.Conv2d(hidden_features, out_features, 1) # 1x1
+        self.drop = nn.Dropout(drop)
+        self.apply(self._init_weights)
+
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.dwconv(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
 
 class TAUSubBlock(nn.Module):
     """A TAUBlock (tau) for Temporal Attention Unit"""
@@ -153,18 +186,18 @@ class TAUSubBlock(nn.Module):
         self.attn = TemporalAttention(dim, kernel_size)
 
         # TODO Understand what it is
-        # self.norm2 = nn.BatchNorm2d(dim)
-        # mlp_hidden_dim = int(dim * mlp_ratio)
-        # self.mlp = MixMlp(
-        #     in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.norm2 = nn.BatchNorm2d(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = MixMlp(
+            in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
 
         self.layer_scale_1 = nn.Parameter(init_value * torch.ones((dim)), requires_grad=True)
-        # self.layer_scale_2 = nn.Parameter(init_value * torch.ones((dim)), requires_grad=True)
+        self.layer_scale_2 = nn.Parameter(init_value * torch.ones((dim)), requires_grad=True)
 
 
     def forward(self, x):
         x = x + self.layer_scale_1.unsqueeze(-1).unsqueeze(-1) * self.attn(self.norm1(x))
-        # x = x + self.layer_scale_2.unsqueeze(-1).unsqueeze(-1) * self.mlp(self.norm2(x))
+        x = x + self.layer_scale_2.unsqueeze(-1).unsqueeze(-1) * self.mlp(self.norm2(x))
         return x
 
 
